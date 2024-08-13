aea7194b9b16d45d0a78027ee81c19c97e727fb6
maphuti2 <maphuti22r@gmail.com>
2024-08-13 22:53:13 +0200

    function that generate random choices by computer

--- PaperRockScissor/index.js
+++ PaperRockScissor/index.js
@@ -1,1 +1,19 @@
-console.log("hello guys")
+//Declaring paper, rock and scissor
+const paper = "paper";
+const rock = "rock";
+const scissor = "scissor";
+
+//creating function that will randomly pick rock, paper or scissor
+
+function getComputerChoice(){
+    const compChoice = Math.floor(Math.random() * (4-1)+1);
+
+    if (compChoice === 1){
+        return "paper";
+    }
+    else if (compChoice === 2){
+        return "rock";
+    }else{
+        return "scissor";
+    }
+}
